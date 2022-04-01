a58d6a8a145b58a4114373e7ae0b41e58ce7795f
Hemanth <b.hemanth1997@gmail.com>
2022-04-01 17:30:00 +0530

    PushClass

---
+++ PushToGit/src/com/push/PushClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.push;
+
+/**
+ * @author chinni
+ *
+ */
+public class PushClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
